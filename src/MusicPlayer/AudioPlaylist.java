efda4ced6f90b82403863cbd6e88133f79bbc8b3
naveenram00 <naveenram00@gmail.com>
2016-03-31 18:50:10 -0700

    Commit 4

--- src/MusicPlayer/AudioPlaylist.java
+++ src/MusicPlayer/AudioPlaylist.java
@@ -3,11 +3,13 @@ package MusicPlayer;
 import javafx.application.*;
 import javafx.beans.value.*;
 import javafx.collections.*;
+import javafx.embed.swing.SwingNode;
 import javafx.event.*;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.MapValueFactory;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.image.*;
 import javafx.scene.layout.*;
 import javafx.scene.media.*;
@@ -15,20 +17,49 @@ import javafx.stage.Stage;
 import javafx.util.Callback;
 import javafx.util.Duration;
 
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.*;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JWindow;
+import javax.swing.SwingConstants;
 
 /** Example of playing all audio files in a given directory. */
 public class AudioPlaylist extends Application {
 	
 	
 	ArrayList<String> paths = new ArrayList<String>();
+	ArrayList<File> songFiles = new ArrayList<File>();
+	ArrayList<File> queueFiles = new ArrayList<File>();
 	ArrayList<String> songs = new ArrayList<String>();
 	ArrayList<String> artists = new ArrayList<String>();
+	public BufferedImage image;
 	
-	private static final String MUSIC_DIR = "/Users/naveen/Music/iTunes/iTunes Media/Music";
+	public static final String SPLASH_IMAGE =
+            "http://fxexperience.com/wp-content/uploads/2010/06/logo.png";
+	private Pane splashLayout;
+    private ProgressBar loadProgress;
+    private Label progressText;
+    private static final int SPLASH_WIDTH = 676;
+    private static final int SPLASH_HEIGHT = 227;
+	
+	public static String libraryFolder = "";
+    private static final String MUSIC_DIR = "/Users/naveen/Music/iTunes/iTunes Media/Music/Black Sabbath/Paranoid";
 	public static final String TAG_COLUMN_NAME = "Tag";
 	public static final String VALUE_COLUMN_NAME = "Value";
 	public static final List<String> SUPPORTED_FILE_EXTENSIONS = Arrays.asList(".mp3", ".m4a");
@@ -42,17 +73,74 @@ public class AudioPlaylist extends Application {
 
   public static void main(String[] args) throws Exception { 
 	 
-	  //launch(args); // "/Users/naveen/Desktop/Library1"
+	  launch(args); // "/Users/naveen/Desktop/Library1"
 	  
 	  AudioPlaylist a = new AudioPlaylist();
 	  a.GetAllFiles("/Users/naveen/Music/iTunes/iTunes Media/Music");
-	  System.out.println(a.songs.get(700));
-	  System.out.println(a.artists.get(700));
+	  System.out.println(a.songs.get(2));
+	  System.out.println(a.artists.get(2));
 	  
   
   }
+  public void init() throws IOException {
+	  image = ImageIO.read(new File("/Users/naveen/Desktop/splashImage.png"));
+	  JLabel icon = new JLabel(new ImageIcon(image));
+	  icon.setLayout(new FlowLayout(FlowLayout.CENTER));
+	  
+	  JWindow window = new JWindow();
+	  window.setBackground(Color.white);
+	  window.setSize(1000, 500);
+	 
+	  
+	  
+	  window.setBounds(150, 100, 1000, 500);
+	  
+	  JButton chooseLibrary = new JButton("Choose Library");
+	    chooseLibrary.addActionListener(new ActionListener() {
+	    	
+		      public void actionPerformed(ActionEvent ae) {
+		        JFileChooser fileChooser = new JFileChooser();
+		        fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+		        
+		        int returnValue = fileChooser.showOpenDialog(null);
+		        if (returnValue == JFileChooser.APPROVE_OPTION) {
+		          File selectedFile = fileChooser.getSelectedFile();
+		          libraryFolder = selectedFile.getPath();
+		         
+		         
+		          }
+		        }
+		      }
+		    );
+	  
+	   
+	  
+	  
+	  
+	  icon.add(chooseLibrary);
+	  window.getContentPane().add(icon);
+	  window.setVisible(true);
+	  chooseLibrary.setLocation(177, 215);
+	  chooseLibrary.setSize(150, 50);
+	  chooseLibrary.setVisible(true);
+	  try {
+	      Thread.sleep(5000);
+	  } catch (InterruptedException e) {
+	      e.printStackTrace();
+	  }
+	
+	  window.setVisible(false);
+	  window.dispose();
+	
+      
+  }
 
   public void start(final Stage stage) throws Exception {
+	  final SwingNode swingNode = new SwingNode();
+      JPanel panel = new JPanel();
+      panel.setSize(400, 400);
+      panel.setVisible(true);
+      swingNode.setContent(panel);
     stage.setTitle("Simple Audio Player");
 
     // determine the source directory for the playlist (either the first argument to the program or a default).
@@ -75,6 +163,8 @@ public class AudioPlaylist extends Application {
             return true;
           }
         }
+        
+        
 
         return false;
       }
@@ -91,6 +181,7 @@ public class AudioPlaylist extends Application {
     final MediaView mediaView = new MediaView(players.get(0));
     final Button skip = new Button("Skip");
     final Button play = new Button("Pause");
+    
 
     // play each audio file in turn.
     for (int i = 0; i < players.size(); i++) {
@@ -108,7 +199,7 @@ public class AudioPlaylist extends Application {
     }
     
     // allow the user to skip a track.
-    skip.setOnAction(new EventHandler<ActionEvent>() {
+    /*skip.setOnAction(new EventHandler<ActionEvent>() {
       @Override public void handle(ActionEvent actionEvent) {
         final MediaPlayer curPlayer = mediaView.getMediaPlayer();
         curPlayer.currentTimeProperty().removeListener(progressChangeListener);
@@ -133,7 +224,8 @@ public class AudioPlaylist extends Application {
         }
       }
     });
-
+    */
+    
     // display the name of the currently playing track.
     mediaView.mediaPlayerProperty().addListener(new ChangeListener<MediaPlayer>() {
       @Override public void changed(ObservableValue<? extends MediaPlayer> observableValue, MediaPlayer oldPlayer, MediaPlayer newPlayer) {
@@ -202,6 +294,7 @@ public class AudioPlaylist extends Application {
 
     // layout the scene.
     final StackPane layout = new StackPane();
+    layout.getChildren().add(swingNode);
     layout.setStyle("-fx-background-color: cornsilk; -fx-font-size: 20; -fx-padding: 20; -fx-alignment: center;");
 
     final HBox progressReport = new HBox(10);
@@ -214,6 +307,8 @@ public class AudioPlaylist extends Application {
         progressReport,
         metadataTable
     );
+    
+  
 
     layout.getChildren().addAll(
       invisiblePause,
@@ -243,67 +338,11 @@ public class AudioPlaylist extends Application {
     		int endSong = 0;
 	    	
 	    	if (file.getCanonicalPath().toString().contains(".")) {
+	    		
 	    		String filepath = file.getCanonicalPath().toString();
-	    		int slashCount = 0;
-	    		int Counter = 0;
-	    		
-	    		/*
-	    		for (int i = 0; i <= filepath.length()-1; i++) {
-	    			
-	    			String check = filepath.substring(i, i+1);
-	    			
-	    			if (check.equalsIgnoreCase("/")) {
-	    			
-	    				slashCount++;
-	    			
-	    		}
-	    	}
-	    		
-	    		for (int j = 0; j < filepath.length()-1; j++) {
-	    			
-	    			if (filepath.substring(j, j+1).equalsIgnoreCase("/")) {
-		    			
-	    				Counter++;
-	    				
-	    			
-	    		}
-	    			if (Counter == slashCount-3){
-	    				beginArtist = j+1;
-	    				
-	    			}
-	    			if (Counter == slashCount-2){
-	    				endArtist = j;
-	    				
-	    			}
-	    			
-	    		}
-	    		
-
-	    		for (int k = 0; k < filepath.length()-1; k++) {
-	    			
-	    			if (filepath.substring(k, k+1).equalsIgnoreCase("/")) {
-		    			
-	    				Counter++;
-	    				
-	    			
-	    		}
-	    			if (Counter == slashCount-3){
-	    				beginSong = k+1;
-	    				
-	    			}
-	    			if (Counter == slashCount-2){
-	    				endSong = k;
-	    				
-	    			}
-	    			
-	    		}
 	    		
 	    		
-	    		songs.add(filepath.substring(beginSong, filepath.length()-1));
-	    		artists.add(filepath.substring(beginArtist, endArtist));
-	    		*/
-	    		
-	    		//System.out.println("File: " + file.getCanonicalPath().toString());
+	    		songFiles.add(file);
 	    		paths.add(filepath);
 	    		artists.add(file.getParentFile().getParentFile().getName());
 	    		songs.add(file.getName());
